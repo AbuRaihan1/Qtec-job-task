dc50fb2696d6a7a202795d252660029e04d416b9
Abu Raihan <raihanahmed01973@gmail.com>
2024-01-29 22:24:32 +0600

    updated navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const navIcon = document.querySelector(".nav-icon");
+const closeIcon = document.querySelector(".close-icon");
 navIcon.addEventListener("click", function () {
   const navContent = document.querySelector(".navbar-links-wrapper");
   navContent.classList.toggle("active-nav");
+  navIcon.style.display = "none";
+  closeIcon.style.display = "block";
+});
+
+closeIcon.addEventListener("click", function () {
+  const navContent = document.querySelector(".navbar-links-wrapper");
+  navContent.classList.toggle("active-nav");
+  closeIcon.style.display = "none";
+  navIcon.style.display = "block";
 });
